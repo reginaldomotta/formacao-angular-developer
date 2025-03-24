1b9226c34c2ea94912d6d6ed42128a8c023091cd
Reginaldo Motta <62575581+reginaldomotta@users.noreply.github.com>
2025-03-24 18:48:26 -0400

    ts-node-dev

--- c2-typescript para angular/src/index.ts
+++ c2-typescript para angular/src/index.ts
@@ -1,12 +1,2 @@
-//generics
-
-function concatArray<T>(...itens: T[]): T[] {
- return new Array().concat(...itens)
-}
-
-const numArray = concatArray<number[]>([1, 5], [3])
-const stgArray = concatArray<string[]>(['felipe', 'goku'], ['vegeta'])
-
-
-console.log(numArray)
-console.log(stgArray)
+let dado: string = 'motta'
+console.log(dado)
