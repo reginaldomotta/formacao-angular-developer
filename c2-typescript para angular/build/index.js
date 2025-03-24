b7cd4d5723cefdcb4770c1a23a6e5531e5e25e38
Reginaldo Motta <62575581+reginaldomotta@users.noreply.github.com>
2025-03-24 18:44:02 -0400

    generics

--- c2-typescript para angular/build/index.js
+++ c2-typescript para angular/build/index.js
@@ -1,21 +1,10 @@
 "use strict";
-class Character {
-    constructor(name, stregth, skill) {
-        this.name = name;
-        this.stregth = stregth;
-        this.skill = skill;
-    }
-    attack() {
-        console.log(`Attack with ${this.stregth} points`);
-    }
+//generics
+function concatArray(...itens) {
+    return new Array().concat(...itens);
 }
-class Magician extends Character {
-    constructor(name, stregth, skill, magicPoints) {
-        super(name, stregth, skill);
-        this.magicPoints = magicPoints;
-    }
-}
-const p1 = new Character('ryu', 10, 98);
-p1.attack();
-const p2 = new Magician('mago', 9, 30, 100);
-console.log(p2);
+const numArray = concatArray([1, 5], [3]);
+const stgArray = concatArray(['felipe', 'goku'], ['vegeta']);
+numArray.push(6);
+console.log(numArray);
+console.log(stgArray);
--- c2-typescript para angular/src/index.ts
+++ c2-typescript para angular/src/index.ts
@@ -1,34 +1,12 @@
-class Character {
- protected name: string
- protected stregth: number
- protected skill: number
+//generics
 
- constructor(name: string, stregth: number, skill: number) {
-  this.name = name
-  this.stregth = stregth
-  this.skill = skill
- }
-
- attack(): void {
-  console.log(`Attack with ${this.stregth} points`)
- }
+function concatArray<T>(...itens: T[]): T[] {
+ return new Array().concat(...itens)
 }
 
-class Magician extends Character {
- magicPoints: number
- constructor(
-  name: string,
-  stregth: number,
-  skill: number,
-  magicPoints: number
- ) {
-  super(name, stregth, skill)
-  this.magicPoints = magicPoints
- }
-}
+const numArray = concatArray<number[]>([1, 5], [3])
+const stgArray = concatArray<string[]>(['felipe', 'goku'], ['vegeta'])
 
-const p1 = new Character('ryu', 10, 98)
-p1.attack()
 
-const p2 = new Magician('mago', 9, 30, 100)
-console.log(p2)
+console.log(numArray)
+console.log(stgArray)
