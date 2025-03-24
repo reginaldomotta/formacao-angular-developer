952a4ceb4a58ce1eb57a3c20f24af3b4e90586e1
Reginaldo Motta <62575581+reginaldomotta@users.noreply.github.com>
2025-03-24 18:54:36 -0400

    decorators - 1

--- c2-typescript para angular/src/index.ts
+++ c2-typescript para angular/src/index.ts
@@ -1,2 +1,14 @@
-let dado: string = 'motta'
-console.log(dado)
+//decorators
+function ExibirNome(target: any){
+  console.log(target)
+}
+
+@ExibirNome
+class Funcionario{
+
+}
+
+@ExibirNome
+class Quincas {
+
+}
